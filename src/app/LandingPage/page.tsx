a6278a45cac96a626afbc397444fa323a9b4cfb9
ankit kuntal <ankitkuntal904@gmail.com>
2025-12-16 16:52:15 +0530

    kingaaaa-16/12/26

---
+++ src/app/LandingPage/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+    <div>king aaaaaaaa</div></>
+
+  );
+}
